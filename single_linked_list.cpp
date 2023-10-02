ded1676c6c62ae1f3c7da888dd7139dc11a24a16
Richard <yisin11012@gmail.com>
2023-10-02 23:13:12 +0800

    first commit

---
+++ single_linked_list.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+struct LinkedList {
+    Node* head;
+
+    LinkedList() {
+        head = nullptr;
+    }
+
+    void add(int data) {
+        Node* newNode = new Node(data);
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
+
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+
+    void show() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "nullptr" << endl;
+    }
+
+    ~LinkedList() {
+        while (head != nullptr) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+
+    LinkedList myList;
+
+    myList.add(1);
+    myList.add(2);
+    myList.add(3);
+    myList.show();
+
+    return 0;
+}
